32cb7512b6c208a48807272a17e36964fd130641
ApprehensiveKnee <edoardo.cabiati@mail.polimi.it>
2024-09-13 18:16:53 +0200

    additions

--- scripts/extract_opt_scan.py
+++ scripts/extract_opt_scan.py
@@ -18,7 +18,6 @@ def extract_data(filename):
             start_index = i+1
         # If the line starts with Leave Link, and i is > start_index, then we have reached the end of the scan
         if re.match(r' Leave Link', line) and start_index is not None:
-            print(line)
             end_index = i
             break
 
@@ -36,11 +35,9 @@ def extract_data(filename):
         if re.match(r'\s+\d+', line):
             parts = line.split()
             steps.extend(map(int, parts))
-            print(steps)
             continue
         if "Eigenvalues --" in line:
             eigenvalues.extend(map(float, line.split()[2:]))
-            print (eigenvalues)
             continue
         elif re.match(r'\s+\w+', line):
             # If the line is not slipced dy spaces, it may be by +/- signs, so we first try to split by spaces
@@ -48,7 +45,6 @@ def extract_data(filename):
             
             parts = re.split(r'\s+|(?<=[0-9])-(?=[0-9])', line)
             current_key = parts[1]
-            print(current_key)
             parts = parts[2:-1]
             # Add the key to the variables dictionary if it is not already there
             if current_key not in variables:
@@ -58,7 +54,6 @@ def extract_data(filename):
                 variables[current_key].extend(map(float, parts))
             except:
                 continue
-            print(variables)
             continue
         else:
             continue
@@ -78,13 +73,14 @@ if __name__ == "__main__":
         # Plot the eigenvalues for the steps over the values of the scan variable
         # Convert eigenvalues from A.U. to kcal/mol
         eigenvalues = [x*627.509 for x in eigenvalues]
-        print(scan_variable)
         figure,axis = plt.subplots(figsize=(10,6))
         axis.plot(variables[scan_variable], eigenvalues, marker='d', linestyle='-', color='coral', markerfacecolor='black')
         axis.set_xlabel(scan_variable)
         axis.set_ylabel("Energy (kcal/mol)")
         axis.grid()
         # Save the plot to a file  
+        # Remove the extension from the filename
+        filename = filename.split(".")[0]
         plt.savefig(f"{filename}.png")
 
 
@@ -100,9 +96,11 @@ if __name__ == "__main__":
         # Plot the scan_variable1 vs scan_variable2 for both files
         figure,axis = plt.subplots(figsize=(10,6))
         axis.plot(variables2[scan_variable1], variables1[scan_variable1], marker='d', linestyle='-', color='coral', markerfacecolor='black',label=scan_variable1)
-        axis.plot(variables2[scan_variable2], variables1[scan_variable2], marker='x', linestyle='-', color='slateblue', markerfacecolor='black', label=scan_variable2)
+        axis.plot(variables2[scan_variable2], variables1[scan_variable2], marker='X', linestyle='-', color='slateblue', markerfacecolor='black', label=scan_variable2)
         axis.set_xlabel(scan_variable1)
         axis.set_ylabel(scan_variable2)
+        axis.legend()
+        axis.grid()
         # Save the plot to a file
-        plt.savefig(f"{filename1}_{filename2}.png")
+        plt.savefig("PES-2d.png")
 
